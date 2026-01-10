d0426cc2f70e6e6aa7e9413479b21717c66d9ee4
Amol Kare <kareamol18@gmail.com>
2026-01-10 15:59:19 +0530

    final push

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:5000/api", // Adjust as needed
+  baseURL: "https://chirplt.onrender.com/", // Adjust as needed
   withCredentials: true, // Ensures cookies are sent
 });
 
